062ca418953c66880e95d1ab97154accd5efdbba
R4hman <rehmanmysayev@gmail.com>
2023-08-22 01:24:45 +0400

    Custom date picker

---
+++ script.js
@@ -0,0 +1,230 @@
+const currMonth = document.querySelector(".current-month");
+
+const prevMonthBtn = document.querySelector(".prev-month");
+const nextMonthBtn = document.querySelector(".next-month");
+const dateDays = document.querySelector(".date-days");
+const sun = document.querySelector(".sun-container");
+const mon = document.querySelector(".mon-container");
+const tue = document.querySelector(".tue-container");
+const wed = document.querySelector(".wed-container");
+const thu = document.querySelector(".thu-container");
+const fri = document.querySelector(".fri-container");
+const sat = document.querySelector(".sat-container");
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
+
+const date = new Date().toLocaleDateString();
+const today = date.split("/")[1];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function setMonth(side) {
+  if (side === "prev") {
+    currentMonth -= 1;
+    if (months[currentMonth] !== undefined) {
+      currMonth.innerText = `${months[currentMonth]}  ${currentYear}`;
+    } else {
+      currentMonth = 11;
+      currentYear -= 1;
+      currMonth.innerText = `${months[currentMonth]}  ${currentYear}`;
+    }
+  }
+  if (side === "next") {
+    {
+      currentMonth += 1;
+      if (months[currentMonth] !== undefined) {
+        currMonth.innerText = `${months[currentMonth]}  ${currentYear}`;
+      } else {
+        currentMonth = 0;
+        currentYear += 1;
+        currMonth.innerText = `${months[currentMonth]}  ${currentYear}`;
+      }
+    }
+  }
+  const date2 = new Date(currentYear, currentMonth, 1);
+  const dateForNextMonth = new Date(currentYear, currentMonth + 1, 1);
+  const days = (dateForNextMonth - date2) / (24 * 3600) / 1000;
+  if (
+    currentMonth === +date.split("/")[0] - 1 &&
+    currentYear === +date.split("/")[2]
+  ) {
+    createDays(days, today);
+  } else createDays(days);
+}
+
+function createDays(days, activeDay) {
+  sun.innerText =
+    mon.innerText =
+    tue.innerText =
+    wed.innerText =
+    thu.innerText =
+    fri.innerText =
+    sat.innerText =
+      "";
+  const arr = Array.from({ length: days }, (x, i) => i + 1);
+  arr.forEach((item) => {
+    /// day 1 yeni ayin birinci gunu hansi hefte gunune dusurse ondan evvelki hefte gunlerine aid gunleri 1rem asagi salmaq
+    const date = new Date(currentYear, currentMonth, item)
+      .toDateString()
+      .split(" ")[0];
+    item === 1 && setAnotherDaysToBeDownOnTheScreen(date);
+
+    let el = getWeekDay(date);
+
+    const span = document.createElement("span");
+    span.classList.add("date-day");
+    if (item == activeDay) {
+      span.classList.add("active");
+    }
+    span.innerText = item;
+    return el.insertAdjacentElement("beforeend", span);
+  });
+}
+
+/// day 1 yeni ayin birinci gunu hansi hefte gunune dusurse ondan evvelki hefte gunlerine aid gunleri 1rem asagi salmaq funksiya
+function setAnotherDaysToBeDownOnTheScreen(date) {
+  switch (date) {
+    case "Sun":
+      resetPositions();
+      break;
+    case "Mon":
+      resetPositions();
+      sun.style.position = "relative";
+      sun.style.top = "83px";
+      break;
+    case "Tue":
+      resetPositions();
+      sun.style.position = mon.style.position = "relative";
+      sun.style.top = mon.style.top = "83px";
+      break;
+    case "Wed":
+      resetPositions();
+      sun.style.position = mon.style.position = tue.style.position = "relative";
+      sun.style.top = mon.style.top = tue.style.top = "83px";
+      break;
+    case "Thu":
+      resetPositions();
+      sun.style.position =
+        mon.style.position =
+        tue.style.position =
+        wed.style.position =
+          "relative";
+      sun.style.top = mon.style.top = tue.style.top = wed.style.top = "83px";
+      break;
+    case "Fri":
+      resetPositions();
+      sun.style.position =
+        mon.style.position =
+        tue.style.position =
+        wed.style.position =
+        thu.style.position =
+          "relative";
+      sun.style.top =
+        mon.style.top =
+        tue.style.top =
+        wed.style.top =
+        thu.style.top =
+          "83px";
+      break;
+    case "Sat":
+      resetPositions();
+      sun.style.position =
+        mon.style.position =
+        tue.style.position =
+        wed.style.position =
+        thu.style.position =
+        fri.style.position =
+          "relative";
+      sun.style.top =
+        mon.style.top =
+        tue.style.top =
+        wed.style.top =
+        thu.style.top =
+        fri.style.top =
+          "83px";
+      break;
+  }
+}
+
+function resetPositions() {
+  sun.style.position =
+    mon.style.position =
+    tue.style.position =
+    wed.style.position =
+    thu.style.position =
+    fri.style.position =
+    sat.style.position =
+      "relative";
+  sun.style.top =
+    mon.style.top =
+    tue.style.top =
+    wed.style.top =
+    thu.style.top =
+    fri.style.top =
+    sat.style.top =
+      "1rem";
+}
+
+function getWeekDay(day) {
+  switch (day) {
+    case "Sun":
+      el = document.querySelector(".sun-container");
+      break;
+
+    case "Mon":
+      el = document.querySelector(".mon-container");
+      break;
+    case "Tue":
+      el = document.querySelector(".tue-container");
+      break;
+    case "Wed":
+      el = document.querySelector(".wed-container");
+      break;
+    case "Thu":
+      el = document.querySelector(".thu-container");
+      break;
+    case "Fri":
+      el = document.querySelector(".fri-container");
+      break;
+    case "Sat":
+      el = document.querySelector(".sat-container");
+      break;
+  }
+  return el;
+}
+
+//sehife ilk defe yuklenende tarixi secib day-leri daxil elesin
+function initialDate() {
+  let initialCurrentMonth = new Date().getMonth();
+  let initialCurrentYear = new Date().getFullYear();
+  const initialDate = new Date(currentYear, currentMonth, 1);
+  const initialDateForNextMonth = new Date(currentYear, currentMonth + 1, 1);
+  const days = (initialDateForNextMonth - initialDate) / (24 * 3600) / 1000;
+  createDays(days, today);
+}
+
+prevMonthBtn.addEventListener("click", () => {
+  setMonth("prev");
+});
+
+nextMonthBtn.addEventListener("click", () => {
+  setMonth("next");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  currMonth.innerText = `${months[currentMonth]}  ${currentYear}`;
+  initialDate();
+});
